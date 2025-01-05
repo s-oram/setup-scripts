fbfa96643e6251f690ec2644d7a3f6fc64c9e6ab
Shannon Oram <shannon.oram@gmail.com>
2025-01-06 10:58:00 +1100

    Update scripts

--- ubuntu/install-server.bash
+++ ubuntu/install-server.bash
@@ -5,4 +5,10 @@
 TOOLS_URL="https://raw.githubusercontent.com/s-oram/setup-scripts/refs/heads/main/ubuntu"
 CACHE_BREAK=$(date +%s)
 
+sudo apt update
+sudo apt-get install wget --yes
+
 wget --quiet "$TOOLS_URL/fish--install.bash?$CACHE_BREAK" -O - | bash
+
+echo ""
+echo "Setup complete, please exit and restart your SSH session"
