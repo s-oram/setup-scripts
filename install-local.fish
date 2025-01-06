6011f7cafa3724e9d9beeae42c691b2424bfcdb4
Shannon Oram <shannon.oram@gmail.com>
2025-01-06 12:27:45 +1100

    Update script

---
+++ install-local.fish
@@ -0,0 +1,13 @@
+# Installs the everything needed:
+# Run:
+# curl --silent --show-error "https://raw.githubusercontent.com/s-oram/setup-scripts/refs/heads/main/install-local.fish" | fish
+
+set TOOLS_URL https://raw.githubusercontent.com/s-oram/setup-scripts/refs/heads/main
+set CACHE_BREAK date +%s
+
+set FUNCTIONS_DIR ~/.config/fish/functions
+
+wget "$TOOLS_URL/mac/fish-functions/rc.fish?$CACHE_BREAK" -O "$FUNCTIONS_DIR/rc.fish"
+
+## wget --quiet "$TOOLS_URL/ghostty--fix.fish?$CACHE_BREAK" -O - | fish
+
---
+++ mac/fish-functions/rc.fish
@@ -0,0 +1,25 @@
+function rc -d "Ripcord"
+
+  function run_remote_script
+    # remove function from global scope
+    functions -e run_remote_script
+
+    set TOOLS_URL https://raw.githubusercontent.com/s-oram/setup-scripts/refs/heads/main
+    set SYSTEM mac
+    set CACHE_BREAK (date +%s)
+    set FILENAME $argv[1]
+
+    # TODO: Report errors
+    wget --quiet "$TOOLS_URL/$SYSTEM/$FILENAME.fish?$CACHE_BREAK" -O - | fish
+  end
+
+
+  # echo $CACHE_BREAK
+
+  switch (count $argv)
+    case 0
+      echo "Choose the name of the script"
+    case '*'
+      run_remote_script $argv[(count $argv)]
+  end
+end
