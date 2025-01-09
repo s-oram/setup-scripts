08888f61b4b1bf58b71b316510baedd48fb80551
Shannon Oram <shannon.oram@gmail.com>
2025-01-10 08:25:41 +1100

    Update scripts

---
+++ mac/setup-server.fish
@@ -0,0 +1,50 @@
+# Sets up Ripcord on the remote host
+#
+# Usage:
+#    rc user@host
+
+#==================#
+#  Read Arguments  #
+#==================#
+set host $argv[1]
+
+
+
+
+#====================#
+#  Define Functions  #
+#====================#
+
+# When using Ghostty to SSH into another machine, you
+# may see errors because those machines do not have
+# Ghostty's terminfo entry.
+#
+# This function copies the local Ghostty terminfo to the
+# remove machine
+function applyGhosttyFix
+  set host $argv[1]
+  infocmp -x | ssh $host -- tic -x -
+end
+
+# Connect to the remote machine and
+# 1. Install Fish
+# 2. Copy Ripcord function to fish functions directory
+function installFishAndRipcord
+  set host $argv[1]
+  ssh $host 'curl --silent --show-error "https://raw.githubusercontent.com/s-oram/setup-scripts/refs/heads/main/install-server.bash" | bash'
+end
+
+
+#=====================#
+#  Run Setup Actions  #
+#=====================#
+
+applyGhosttyFix $host
+
+installFishAndRipcord
+
+
+
+
+
+
